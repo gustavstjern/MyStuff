ebf78d563ff23e391454dec228f3e39e2dd6502e
Gustav Stjernlof <gustav.stjernlof@hotmail.com>
2016-09-16 15:15:23 +0200

    forloop

--- forloop/Program.cs
+++ forloop/Program.cs
@@ -10,6 +10,55 @@ namespace forloop
     {
         static void Main(string[] args)
         {
+            //Övn1
+            /*
+            for(int i = 89; i<=100; i=i+1)
+            {
+                Console.WriteLine(i);
+            }
+            */
+            //Övn2
+            /*
+            for (int i = 20; i > 0; i = i - 1)
+            {
+                Console.WriteLine(i);
+            }
+            */
+            //Övn3
+            /*
+            for(int i=50; i>=2; i = i - 2)
+            {
+                Console.WriteLine(i);
+            }
+            */
+            //Övn1
+            /*
+            Console.Write("Mata in start: ");
+            int tal1 = int.Parse(Console.ReadLine());
+            Console.Write("Mata in stop: ");
+            int tal2 = int.Parse(Console.ReadLine());
+            Console.Write("Mata in steg: ");
+            int tal3 = int.Parse(Console.ReadLine());
+            for (int i=tal1; i <= tal2; i = i + tal3)
+            {
+                Console.WriteLine(i);
+            }
+            */
+            //Övn2
+            /*
+            Console.Write("Mata in ett tal: ");
+            int tal1 = int.Parse(Console.ReadLine());
+            Console.Write("Summan av alla tal från 1 till " + tal1 + " blir: ");
+            int y = 0;
+            for (int i = 0; i <= tal1; i++)
+            {
+                y = y + i;
+            }
+            Console.WriteLine(y);
+            */
+            //Övn3
+            Console.WriteLine("Mata in ett heltal: ");
+            
         }
     }
 }
